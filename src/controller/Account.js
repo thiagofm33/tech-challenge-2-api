c61736982343d0e501e6f91947fea0671d529991
Thiago Martins <thiagofm33@gmail.com>
2024-12-29 12:01:09 -0300

    Added error handling to the transactions endpoints

--- src/controller/Account.js
+++ src/controller/Account.js
@@ -28,7 +28,7 @@ class AccountController {
       const account = await getAccount({ repository: accountRepository,  userId })
       const transactions = await getTransaction({ filter: { accountId: account[0].id }, repository: transactionRepository })
       const cards = await getCard({ filter: { accountId: account[0].id }, repository: cardRepository })
-    
+
       res.status(200).json({
         message: 'Conta encontrada carregado com sucesso',
         result: {
@@ -42,34 +42,62 @@ class AccountController {
         message: 'Erro no servidor'
       })
     }
-    
+
   }
 
   async createTransaction(req, res) {
     const { saveTransaction, transactionRepository } = this.di
     const { accountId, value, type, from, to } = req.body
-    const transactionDTO = new TransactionDTO({ accountId, value, from, to,  type, date: new Date() })
 
-    const transaction = await saveTransaction({ transaction: transactionDTO, repository: transactionRepository })
-    
-    res.status(201).json({
-      message: 'Transação criada com sucesso',
-      result: transaction
+    const transactionDTO = new TransactionDTO({
+      accountId,
+      value,
+      from,
+      to,
+      type,
+      date: new Date()
     })
+
+    try {
+      const transaction = await saveTransaction({
+        transaction: transactionDTO,
+        repository: transactionRepository
+      })
+
+      res.status(201).json({
+        message: 'Transação criada com sucesso',
+        result: transaction
+      })
+    } catch (error) {
+      res.status(422).json({
+        message: 'Erro ao criar a transação!',
+        result: error
+      })
+    }
   }
 
   async getStatment(req, res) {
     const { getTransaction, transactionRepository } = this.di
-
     const { accountId } = req.params
 
-    const transactions = await getTransaction({ filter: { accountId } ,  repository: transactionRepository})
-    res.status(201).json({
-      message: 'Transação criada com sucesso',
-      result: {
-        transactions
-      }
-    })
+    try {
+      const transactions = await getTransaction({
+        filter: { accountId },
+        repository: transactionRepository
+      })
+
+      res.status(200).json({
+        message: 'Transações localizadas com sucesso',
+        result: {
+          transactions
+        }
+      })
+    } catch(error) {
+      res.status(500).json({
+        message: 'Erro ao carregar transações!',
+        result: error
+      })
+    }
   }
 }
 
